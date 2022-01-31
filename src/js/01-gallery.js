3255f82454e587726d104e34a0b2d947a6247218
Mark Yatsiuk <markiatsuk@gmail.com>
2022-01-31 12:30:57 +0200

    1, 2 дз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,52 @@
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+//создание разметки галереи
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+//обработка события при клике
+galleryEl.addEventListener("click", onGalleryClick);
+//вставка галереи на страницу
+galleryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createImgCardsMarkup(galleryItems){
+    
+    return galleryItems.map(({preview, original, description}) => {
+        return   `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `;
+    }).join('');
+    
+};
+
+//функция обработки при клике
+function onGalleryClick(event){
+    event.preventDefault();
+    const isImgEl = event.target.classList.contains("gallery__image");
+
+    if(!isImgEl){
+        return;
+    }
+
+    openModalSimpleLightbox();
+};
+
+//функция с библиотекой-слайдером
+function openModalSimpleLightbox(){
+    let simpleLightbox = new SimpleLightbox('.gallery a',
+        {
+            captionSelector: 'img',
+            captionType: 'attr',
+            captionsData: 'alt',
+            animationSpeed: 150,
+            captionPosition: 'bottom',
+            captionDelay: 250,
+            enableKeyboard: true,  
+        });
+        simpleLightbox.open();
+};
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,56 @@
+import Player from '@vimeo/player';
 
+import throttle from 'lodash.throttle';
+
+
+// const iframe = document.querySelector('iframe');
+ // Создаем элемент player, библиотеки Player
+// const player = new Player(iframe);
+
+
+const iframe = document.querySelector("iframe");
+
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data){
+    const time = data.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+}
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function(){
+
+
+}).catch(function(error){
+    switch (error.name){
+        case 'RangeError':
+            break;
+            default: 
+            break;
+
+    }
+});
+
+// Метод player.setCurrentTime(seconds) - устанавливает время старта видео, при запуске плеера
+// аргумент seconds - время в секудах (localStorage.getItem('videoplayer-current-time') - время из локального хранилища)
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time')).then(function() {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});;
+
+//Ф-ция - принимает объект события "timeupdate" ({duration: 61.857, percent: 0.049, seconds: 3.034}),
+//Сохраняет в локальное хранилище текущее время видео (time), ключ - 'videoplayer-current-time'
+// function onPlay(data) {
+//     const time = data.seconds;
+//     localStorage.setItem('videoplayer-current-time', time);
+// }
